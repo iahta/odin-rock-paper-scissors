2648d09bc8162416f190d402e3fe442679358339
iahta <153889016+iahta@users.noreply.github.com>
2024-01-11 20:40:03 -0600

    Play 5 rounds of RPS.

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
-
-let randomWord = Math.floor(Math.random() * 3);
 let i = 0;
-const computerSelection = getComputerChoice(randomWord);
+
+
+
+function game() {
+   while (i <= 4) {
+
+    let randomWord = Math.floor(Math.random() * 3);
+    let computerSelection = getComputerChoice(randomWord);
+    let playerSelection = prompt("Rock, Paper, Scissors?");
+  
+    function getComputerChoice(randomWord){
+        if(randomWord === 0) {
+            return 'Rock';
+        } else if(randomWord === 1) {
+            return 'Paper';
+        } else {
+            return 'Scissors';
+        }
+    } 
+
+    console.log(playerSelection);
+    console.log(computerSelection); 
+
+    
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+            result = "Tie!! Replay the round.";
+        }
+          else {
+                switch(playerSelection.toUpperCase()){
+                    case 'ROCK':
+                        result = (computerSelection === 'Scissors') ? "You win!" : "You Lose!";
+                        i++;
+                        break;
+                    case 'PAPER': 
+                        result = (computerSelection === 'Rock') ? "You win!" : "You Lose!";
+                        i++;
+                        break;
+                    case 'SCISSORS': 
+                        result = (computerSelection === 'Paper') ? "You win!" : "You Lose!";
+                        i++;
+                        break;
+                }
+            }
+            return result;
+          }
+          console.log(playRound(playerSelection, computerSelection));
+        
+    }
+
+}
+
+
+
+
+
+/*let randomWord = Math.floor(Math.random() * 3);
+let computerSelection = getComputerChoice(randomWord);
 let playerSelection = prompt("Rock, Paper, Scissors?");
-console.log(playerSelection);
-console.log(computerSelection); 
-
-
-
 function getComputerChoice(randomWord){
     if(randomWord === 0) {
         return 'Rock';
@@ -17,8 +67,9 @@ function getComputerChoice(randomWord){
         return 'Scissors';
     }
 }
-
-/*function game(i)*/
+game()
+if result win player plus 1 if lose computer plus 1 
+greater result returns winner
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
@@ -38,34 +89,28 @@ function playRound(playerSelection, computerSelection) {
                     break;
             }
         }
-        i++;
         return result;
       }
 
        
       console.log(playRound(playerSelection, computerSelection));
 
-    /*
     else if(playerSelection.toUpperCase() = computerSelection.toUpperCase()){
         return "You Won! Rock beats Scissors.";
     }else (computerSelection === 'Paper')
-        return "You Lose! Paper beats Rock.";*/
+        return "You Lose! Paper beats Rock.";
 
 
-
-
-
-
-/*Tie!  equsl 
+Tie!  equsl 
 You Won Rock Beats Scissors! rock is greater than paper
 You Won Scissor beats paper! Scissors is greater than paper
 You Won Paper beats Rocks! Paper is greateer than paper
 
 You Lose "" 
 You lose ""
-you lose ""*/
+you lose ""
 
-/* 
 1 = Rock
 2 = Sissors 
-3 = Paper */
+3 = Paper 
+*/
